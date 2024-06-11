312fe467d8f9c22d4c788c1f0332267cac27581c
Luis Alcalde <luisalcaldegarcia.02@gmail.com>
2024-06-11 19:01:30 +0200

    nopal añadido
    user: nopal
    passwd: 12345678

--- consults.py
+++ consults.py
@@ -9,7 +9,8 @@ datetime_obj = datetime.strptime(strin, "%Y-%m-%d %H:%M:%S")
 fecha_cierre_actual = datetime_obj.strftime("%Y-%m-%d %H:%M:%S")
 print(fecha_cierre_actual)
 
-res = bd.fetch_data("SELECT * FROM pedidos_historicos WHERE fecha > ?", (fecha_cierre_actual,))
+#res = bd.fetch_data("SELECT * FROM p WHERE fecha > ?", (fecha_cierre_actual,))
+res = bd.fetch_data("SELECT * FROM usuario")
 for i in res:
     print(i)
 
